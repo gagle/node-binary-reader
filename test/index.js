1f1ca22ea29fd1705acf7c68079dc9688d1ceb26
Gabriel Llamas <gabriel_llamas_llopis@yahoo.es>
2013-10-11 20:51:05 +0200

    v0.1.0

--- test/index.js
+++ test/index.js
@@ -218,6 +218,23 @@ var tests = {
 				})
 				.close ();
 	},
+	"cancel": function (done){
+		var r = br.open (file)
+				.on ("error", assert.ifError)
+				.on ("close", function (){
+					assert.ok (true);
+					done ();
+				})
+				.read (1, function (bytesRead, buffer, cb){
+					process.nextTick (function (){
+						r.cancel ();
+					});
+				})
+				.read (1, function (bytesRead, buffer){
+					assert.fail ();
+				})
+				.close ();
+	},
 	"directory": function (done){
 		var r = br.open (".")
 				.on ("error", function (error){
